ed3474ddde8d366273c06d74c7f3d140b8b7b738
Luisa Burini <luisa.burini@gmail.com>
2021-04-27 12:56:44 -0300

    Mudanças letreiro

--- actions.js
+++ actions.js
@@ -1,7 +1,8 @@
 marqueeInit({
 	uniqueid: 'mycrawler',
 	style: {
-    'color': 'darkblue',
+	'color': '#fff',
+	'text-shadow': '0 0 5px #fff, 0 0 10px #fff, 0 0 15px #0073e6, 0 0 20px #0073e6, 0 0 25px #0073e6, 0 0 30px #0073e6, 0 0 35px #0073e6',
     'font-family': 'Arial, Helvetica, sans-serif',
     'font-size': '15vh',
     'line-height': '17vh'
@@ -13,3 +14,9 @@ marqueeInit({
 	persist: true,
 	savedirection: true
 });
+
+
+function hideAll()
+{
+	
+}
