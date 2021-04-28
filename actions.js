3f84ad2662a2d0cc5a76532bd74522cc9471e385
Luisa Burini <luisa.burini@gmail.com>
2021-04-28 00:33:07 -0300

    Varias alteracoes

--- actions.js
+++ actions.js
@@ -15,17 +15,47 @@
 // 	savedirection: true
 // });
 
-var brazilArcade = document.getElementById('brazil_arcade');
+var brazil = document.getElementById('brazil');
+var arcade = document.getElementById('arcade');
 var endless = document.getElementById('endless');
 var lozenge = document.getElementById('lozenge');
 var spanArcade = document.getElementById('span_arcade');
 var maco1 = document.getElementById('maco1');
 var maco2 = document.getElementById('maco2');
 var maco3 = document.getElementById('maco3');
+var insertCoin = document.getElementById('insert_coin');
+var toBuyCentrao = document.getElementById('to_buy_centrao');
+var insertCoinAnimation = document.getElementById('insert_coin_animation')
+var centraoIndex = 0;
+
+var intervalID = window.setInterval(myCallback, 500);
+
+function myCallback(a, b)
+{
+	centraoIndex++;
+	if(centraoIndex % 4 == 0)
+	{
+		insertCoinAnimation.href = "https://brasil.elpais.com/brasil/2021-01-29/quatro-ministerios-e-bilhoes-de-reais-em-emendas-a-receita-de-bolsonaro-para-vencer-no-congresso-de-bracos-dados-com-o-centrao.html";
+	}
+	else if(centraoIndex % 4 == 1)
+	{
+		insertCoinAnimation.href = "https://economia.estadao.com.br/noticias/geral,congresso-ve-bolsonaro-nas-maos-do-centrao-apos-sancao-do-orcamento-com-preservacao-de-emendas,70003691284";
+	} 
+	else if(centraoIndex % 4 == 2)
+	{
+		insertCoinAnimation.href = "https://veja.abril.com.br/blog/sensacionalista/bolsonaro-compra-centrao-e-faz-garantia-estendida/";
+	} 
+	else if(centraoIndex % 4 == 3)
+	{
+		insertCoinAnimation.href = "https://www.bbc.com/portuguese/brasil-52466624";
+	}
+}
+
 
 function hideAll()
 {
-	brazilArcade.style.display = 'none';
+	brazil.style.display = 'none';
+	arcade.style.display = 'none';
 	lozenge.style.display = 'none';
 	spanArcade.style.display = 'none';
 }
@@ -35,6 +65,8 @@ function showAll()
 	maco1.style.display = 'block';
 	maco2.style.display = 'block';
 	maco3.style.display = 'block';
+	insertCoin.style.display = 'block';
+	toBuyCentrao.style.display = 'block';
 }
 
 function start()
